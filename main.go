0cb6823d82b34784c826f6d6ed39e582cb839ddf
skapskap <skaprange@gmail.com>
2024-03-15 16:50:53 -0300

    setup server

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+)
+
 func main()  {
-	
+	r := chi.NewRouter();
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      r,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	fmt.Println("Starting server in port 8080...")
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		fmt.Printf("Server failed to start: %v\n", err)
+	}
 }
